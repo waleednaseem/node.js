580b2b8a6ce44594290a6f57e9b5343f04932c6c
waleed naseem <waleed.naseem1@gmail.com>
2021-06-10 18:32:13 +0500

    .

---
+++ node/node jwt/node.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const App = express()
+const jwt = require('jsonwebtoken')
+const port = 8080
+
+App.post('/api/post', VerifyToken, (req, res) => {
+    jwt.verify(req.token,'secretkey',(err,authData)=>{
+        if(err){
+            res.sendStatus(404)
+        }else{
+            res.json({
+                msg: 'Post created',
+                authData
+            })
+        }
+    })
+})
+App.post('/api/login', (req, res) => {
+    const user = {
+        id: 1,
+        username: 'waleed',
+        email: 'waleed@gmail.com'
+    }
+    jwt.sign({ user: user }, 'secretkey', (err, token) => {
+        res.json({
+            token
+        })
+    })
+})
+
+function VerifyToken(req, res, next) {
+    const barrerheader = req.headers['authorization']
+    if(typeof barrerheader !== 'undefined'){
+        const bearrerToken= barrerheader.split(' ')[1]
+        req.token = bearrerToken
+        next()
+    }else{
+        res.sendStatus(403)
+    }
+}
+App.listen(port, console.log(`server is running on ${port}`))
